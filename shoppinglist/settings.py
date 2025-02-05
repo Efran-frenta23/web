0a99d8d854ec9bbf46d43b58f270f62f44b4321f
efran <efransarunisatu@gmail.com>
2025-02-05 14:16:24 +0700

    web-nambahin granian

--- shoppinglist/settings.py
+++ shoppinglist/settings.py
@@ -12,16 +12,29 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+env = environ.Env(
+    SECRET_KEY=(str,""),
+    DB_NAME=(str,""),
+    DB_HOST=(str,""),
+    DB_PORT=(int,0),
+    DB_USER=(str,""),
+    DB_PASSWORD=(str,""),
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+environ.Env.read_env(os.path.join(BASE_DIR,".env"))
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u)v$l*73-izz=kl0e#i)2ilq854^pyyl2f)3wshma=za5x_($q'
+SECRET_KEY = env("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,8 +90,13 @@ WSGI_APPLICATION = 'shoppinglist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env("DB_NAME"),
+        'HOST': env("DB_HOST"),
+        'PORT': env("DB_PORT"),
+        'USER': env("DB_USER"),
+        'PASSWORD': env("DB_PASSWORD"),
+    
     }
 }
 
